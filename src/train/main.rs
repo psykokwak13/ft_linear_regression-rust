e14e677ba8296b22d35a20ef8322eb4ef80d2ddd
lhommeauplan <enzo.gatien@gmail.com>
2026-04-16 16:29:19 +0200

    change on const learning_rate, floating point passing to 64 bits

--- src/train/main.rs
+++ src/train/main.rs
@@ -1,43 +1,44 @@
 use csv::ReaderBuilder;
 use std::{env, time::{Duration, Instant}};
 
-const TIME_TRAIN : u64 = 10;
-const LEARNING_RATE : f32 = 0.01;
+const TIME_TRAIN : u64 = 60;
+const LEARNING_RATE : f64 = 0.0000000001;
 
-fn estimate_price(mileage : i32, theta : (f32, f32)) -> f32{
-	theta.0 + theta.1 * (mileage as f32)
+fn estimate_price(mileage : i32, theta : (f64, f64)) -> f64{
+	theta.0 + theta.1 * (mileage as f64)
 }
 
-fn summation0(data : &Vec<Vec<i32>>, theta : (f32, f32)) -> f32 {
-	let mut result = 0.0f32;
+fn summation0(data : &Vec<Vec<i32>>, theta : (f64, f64)) -> f64 {
+	let mut result = 0.0f64;
 
 	for d in data.iter() {
-		result += estimate_price(*(d.get(0).expect("number not found")), theta) - *(d.get(1).expect("number not found")) as f32;
+		result += estimate_price(*(d.get(0).expect("number not found")), theta) - *(d.get(1).expect("number not found")) as f64;
 	}
-
 	result
 }
 
-fn summation1(data : &Vec<Vec<i32>>, theta : (f32, f32)) -> f32 {
-	let mut result = 0.0f32;
+fn summation1(data : &Vec<Vec<i32>>, theta : (f64, f64)) -> f64 {
+	let mut result = 0.0f64;
 
 	for d in data.iter() {
-		result += (estimate_price(*(d.get(0).expect("number not found")), theta) - *(d.get(1).expect("number not found")) as f32) * *(d.get(0).expect("number not found")) as f32;
+		result += (estimate_price(*(d.get(0).expect("number not found")), theta) - *(d.get(1).expect("number not found")) as f64) * *(d.get(0).expect("number not found")) as f64;
 	}
-
 	result
 }
 
-fn train(data : Vec<Vec<i32>>) -> (f32, f32) {
-	let mut theta : (f32, f32) = (0.0, 0.0); // theta0, theta1 = 0.0
+fn train(data : Vec<Vec<i32>>) -> (f64, f64) {
+	let mut theta : (f64, f64) = (0.0, 0.0); // theta0, theta1 = 0.0
 
-	let mut now = Instant::now();
+	let now = Instant::now();
 	while now.elapsed().as_secs() != Duration::from_secs(TIME_TRAIN).as_secs() {
-		theta.0 -= LEARNING_RATE * (1.0 / (data.len() as f32)) * summation0(&data, theta) ;
-		theta.1 -= LEARNING_RATE * (1.0 / (data.len() as f32)) * summation1(&data, theta); 
+		theta.0 -= LEARNING_RATE * (1.0 / (data.len() as f64)) * summation0(&data, theta) ;
+		theta.1 -= LEARNING_RATE * (1.0 / (data.len() as f64)) * summation1(&data, theta); 
+		if theta.0.is_nan() || theta.1.is_nan() {
+			println!("<NAN FOUND !!>");
+			break
+		}
 	}
 
-	println!("0 : {}, 1 : {}", theta.0, theta.1);
 	theta
 }
 
@@ -53,8 +54,7 @@ fn main() {
 				.flatten()
 				.map(|x| x.iter().map(|s| s.parse().unwrap()).collect())
 				.collect();
+	let result : (f64, f64) = train(data);
 
-	let mut result : (f32, f32) = train(data);
-
-	println!("voila pour {} : {}", 100000, estimate_price(100000, result));
+	println!("here for {} : {}", 100000, estimate_price(100000, result));
 }
